e31ee98615dc98ffc030c00afe6bb2cc9d76789d
jpaivarinta <jouka.paivarinta@gmail.com>
2020-03-20 00:34:39 +0200

    "Initial design ideas, basic functionalities"

--- app/src/main/java/com/example/diamory/LogActivity.kt
+++ app/src/main/java/com/example/diamory/LogActivity.kt
@@ -1,12 +1,93 @@
 package com.example.diamory
 
+import android.Manifest
+import android.app.Activity
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
+import android.widget.TextView
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_log.*
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
+import java.time.format.FormatStyle
 
 class LogActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_log)
+
+        showDate()
+
+        button_add_image.setOnClickListener{
+            //checking runtime permission
+            if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+                if (checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_DENIED){
+                    //permission is denied
+                    val permissions = arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE)
+                    requestPermissions(permissions, PERMISSION_CODE)
+                } else {
+                    //permission given
+                    pickGalleryImage()
+                }
+
+            } else{
+                //using lower than Android 10.0, no need for permission
+                pickGalleryImage()
+            }
+        }
+
+    }
+    //show current date
+    private fun showDate(){
+        val current = LocalDate.now()
+        val dateFormatter = DateTimeFormatter.ofLocalizedDate(FormatStyle.MEDIUM)
+        val formattedDate = current.format(dateFormatter)
+        val textView: TextView = findViewById(R.id.dateText) as TextView
+        textView.setText(formattedDate)
+
+    }
+
+    //pick image from gallery
+    private fun pickGalleryImage(){
+
+        val intent = Intent(Intent.ACTION_PICK)
+        intent.type = "image/*"
+        startActivityForResult(intent, IMAGE_PICK_CODE)
+    }
+
+
+    companion object{
+        //image pick code
+        private val IMAGE_PICK_CODE = 1000
+        //permission code
+        private val PERMISSION_CODE = 1001
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        when(requestCode){
+            PERMISSION_CODE -> {
+                if(grantResults.size > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                    //permission granted from popup
+                    pickGalleryImage()
+                } else{
+                    Toast.makeText(this, "Permission denied", Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        if(resultCode == Activity.RESULT_OK && requestCode == IMAGE_PICK_CODE){
+            imageView.setImageURI(data?.data)
+        }
     }
 }
