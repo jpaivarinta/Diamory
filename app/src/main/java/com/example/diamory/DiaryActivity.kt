ed7acc9d46e0f50119d71649463711fa8388c5a6
skick <jonne.taipale@gmail.com>
2020-05-05 15:24:03 +0300

    scrollable dates!!!!!!!!!

--- app/src/main/java/com/example/diamory/DiaryActivity.kt
+++ app/src/main/java/com/example/diamory/DiaryActivity.kt
@@ -10,46 +10,38 @@ import java.time.format.DateTimeFormatter
 class DiaryActivity : AppCompatActivity() {
 
     lateinit var listView: ListView
+    private var items = arrayListOf<String>()
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_diary)
 
-        listView = findViewById()
+
+        listView = findViewById<ListView>(R.id.dbListview)
+        items = createList()
+        //val listItems = arrayOfNulls<String>(items.size)
+        //for (i in 0 until items.size) {
+        //    listItems[i] = items[i]
+        //}
+        val adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, items)
+        listView.adapter = adapter
 
 
-        tvDisplayDb.text = ""
+    }
+
+    private fun createList(): ArrayList<String> {
+        var tempList = arrayListOf<String>()
         val dbHandler = SQLiteHelper(this)
         val cursor = dbHandler.getAllLogs()
+
         cursor!!.moveToFirst()
-        tvDisplayDb.append(cursor.getString(cursor.getColumnIndex(SQLiteHelper.KEY_TEXT)))
-        tvDisplayDb.append("\n")
+        tempList.add(cursor.getString(cursor.getColumnIndex(SQLiteHelper.KEY_DATE)))
         while (cursor.moveToNext()) {
-            tvDisplayDb.append(cursor.getString(cursor.getColumnIndex(SQLiteHelper.KEY_DATE)))
-            tvDisplayDb.append("\n")
+            tempList.add(cursor.getString(cursor.getColumnIndex(SQLiteHelper.KEY_DATE)))
         }
         cursor.close()
-
-
-        /*
-        tvDisplayDb.text = ""
-        val dbHandler = SQLiteHelper(this)
-        val cursor = dbHandler.getAllLogs()
-        cursor!!.moveToFirst()
-        tvDisplayDb.append(cursor.getString(cursor.getColumnIndex(SQLiteHelper.KEY_TEXT)))
-        tvDisplayDb.append(" ")
-        tvDisplayDb.append(cursor.getString(cursor.getColumnIndex(SQLiteHelper.KEY_DATE)))
-        tvDisplayDb.append("\n")
-        while (cursor.moveToNext()) {
-            tvDisplayDb.append(cursor.getString(cursor.getColumnIndex(SQLiteHelper.KEY_TEXT)))
-            tvDisplayDb.append(" ")
-            tvDisplayDb.append(cursor.getString(cursor.getColumnIndex(SQLiteHelper.KEY_DATE)))
-            tvDisplayDb.append("\n")
-        }
-        cursor.close()
-
-
-         */
+        return tempList
     }
 
 
